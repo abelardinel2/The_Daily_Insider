6c232fbc9c75d4878d1e53cee856666a0e8b3204
abelardinel2 <ameliabelardinelli@gmail.com>
2025-06-25 08:02:39 -0400

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,8 +1,12 @@
 import requests
+import os
+from dotenv import load_dotenv
 
-# Your Telegram bot token and chat ID
-TELEGRAM_BOT_TOKEN = "7975548444:AAFtmHs3S3GYL_rDpawtDE-f_09_lFg3ex8"
-TELEGRAM_CHAT_ID = "6652085600"
+# Load variables from .env
+load_dotenv()
+
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
 def send_telegram_message(message):
     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
