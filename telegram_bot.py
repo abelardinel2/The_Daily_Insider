07054be520a12096d2d1c0db05f72158c1c00161
abelardinel2 <ameliabelardinelli@gmail.com>
2025-06-27 12:43:50 -0400

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -4,7 +4,7 @@ import requests
 def send_telegram_message(message):
     TELEGRAM_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
     TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
-    
+
     if not TELEGRAM_TOKEN or not TELEGRAM_CHAT_ID:
         raise ValueError("Missing Telegram credentials in environment variables")
 
