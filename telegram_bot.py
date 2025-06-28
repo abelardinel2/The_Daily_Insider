230ccbae3f9f7ae5cad775e3729c7526f6600828
abelardinel2 <ameliabelardinelli@gmail.com>
2025-06-28 09:22:18 -0400

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -5,10 +5,7 @@ def send_telegram_message(message):
     bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
     chat_id = os.getenv("TELEGRAM_CHAT_ID")
     if not bot_token or not chat_id:
-        raise ValueError("Missing Telegram credentials")
-
+        raise ValueError("Telegram info missing.")
     url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
     payload = {"chat_id": chat_id, "text": message}
-    resp = requests.post(url, data=payload)
-    if resp.status_code != 200:
-        raise Exception(f"Telegram error: {resp.text}")
+    requests.post(url, data=payload)
