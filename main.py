171397e4a425c0f91e9880f41fb52f9b9fa9f1b4
abelardinel2 <ameliabelardinelli@gmail.com>
2025-06-28 16:26:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,10 @@
-import os
-from fetcher import fetch_all_form4s
-from notifier import send_summary
+import json
+import notifier
 
 def main():
-    buys, sells = fetch_all_form4s(days=5)
-
-    bias = "Neutral Bias 👀"
-    if buys > sells:
-        bias = "Buy-Side Bias 👀"
-    elif sells > buys:
-        bias = "Sell-Side Bias 👀"
-
-    summary = f"""
-📊 Insider Flow Summary
-
-💰 Top Buys: {buys}
-💥 Top Sells: {sells}
-
-🧮 Total Buys: {buys} | Total Sells: {sells}
-📉 Bias: {bias}
-"""
-    print(summary)
-    send_summary(summary)
+    with open("insider_flow.json") as f:
+        data = json.load(f)
+    notifier.send_summary(data)
 
 if __name__ == "__main__":
     main()
