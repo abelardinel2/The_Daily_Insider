6370e849aa55c24af5b8d629210b6239251c2be4
abelardinel2 <ameliabelardinelli@gmail.com>
2025-06-28 17:29:26 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
+
 import json
-from send_telegram import send_summary  # ✅ use the real filename
+from send_telegram import send_summary
 
 def main():
     with open("insider_flow.json") as f:
         data = json.load(f)
-
     send_summary(data)
 
 if __name__ == "__main__":
