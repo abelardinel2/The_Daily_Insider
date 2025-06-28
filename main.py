0da4bfe8d661cc99f1c1031f209dacd6cfcec326
abelardinel2 <ameliabelardinelli@gmail.com>
2025-06-28 17:33:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-
 import json
-from send_telegram import send_summary
+import send_telegram
 
 def main():
     with open("insider_flow.json") as f:
         data = json.load(f)
-    send_summary(data)
+    send_telegram.send_summary(data)
 
 if __name__ == "__main__":
     main()
