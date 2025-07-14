40b89daa1f7ac7783e9a419af52c6a466c56f365
abelardinel2 <ameliabelardinelli@gmail.com>
2025-07-13 23:53:58 -0400

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,10 +1,10 @@
 import requests
-from src.config import BOT_TOKEN, CHAT_ID
+from src.config import TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
 
 def send_telegram_message(message):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
     payload = {
-        "chat_id": CHAT_ID,
+        "chat_id": TELEGRAM_CHAT_ID,
         "text": message,
         "disable_web_page_preview": True
     }
