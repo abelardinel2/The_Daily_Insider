1b6c2336412c76f90cd52b8df6d503ced52342d1
abelardinel2 <ameliabelardinelli@gmail.com>
2025-07-13 22:38:51 -0400

    Create sec_parser.py

---
+++ sec_parser.py
@@ -0,0 +1,9 @@
+from bs4 import BeautifulSoup
+
+def parse_form4_txt(txt_content):
+    soup = BeautifulSoup(txt_content, "html.parser")
+    tables = soup.find_all("table")
+    for table in tables:
+        if "nonDerivativeTable" in str(table):
+            return True  # Found valid Form 4 data
+    return False
