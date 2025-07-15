a3b7424f708ddd1fead2cd81cf2feabfcf5d8e5c
abelardinel2 <ameliabelardinelli@gmail.com>
2025-07-15 01:06:17 -0400

    clean

--- README.md
+++
@@ -1,3 +0,0 @@
-# Insider Flow Summary Bot
-
-This bot fetches SEC Form 4 insider trading data daily, summarizes total buys/sells, and sends formatted alerts to Telegram.
--- sec_parser.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-
-def parse_form4_txt(txt_content):
-    soup = BeautifulSoup(txt_content, "html.parser")
-    tables = soup.find_all("table")
-    for table in tables:
-        if "nonDerivativeTable" in str(table):
-            return True
-    return False
